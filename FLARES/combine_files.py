1e7282cae24a66872018ec91ceeaba6a29367cc0
louiseseeyave <louise.seeyave@gmail.com>
2024-02-14 10:52:00 +0000

    scripts for no dust or neb emission

--- FLARES/combine_files.py
+++ FLARES/combine_files.py
@@ -9,11 +9,13 @@ snaps = ["000_z015p000", "001_z014p000", "002_z013p000", "003_z012p000",
 
 data_dir = "/cosma7/data/dp004/dc-seey1/modules/synthesizer-pipeline/FLARES/data_indices"
 
-master_f = h5py.File(f"data_indices/flares_photometry_indices_{region}.hdf5", 'a')
+# master_f = h5py.File(f"data_indices/flares_photometry_indices_{region}.hdf5", 'a')
+master_f = h5py.File(f"data_indices/flares_photometry_cloudy-hd_{region}.hdf5", 'a')
 
 for snap in snaps:
 
-    fname = f"flares_photometry_indices_{region}_{snap}.hdf5"
+    # fname = f"flares_photometry_indices_{region}_{snap}.hdf5"
+    fname = f"flares_photometry_cloudy-hd_{region}_{snap}.hdf5"
 
     snap_f = h5py.File(f"{data_dir}/{fname}", 'r')
     
